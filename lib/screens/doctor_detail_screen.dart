b5f6345a08cf399dfa68a9e291af62823393db36
mhefny1995 <mhefny1995@users.noreply.github.com>
2026-02-11 12:59:37 +0200

    Fix YAML and Dart import errors

--- lib/screens/doctor_detail_screen.dart
+++ lib/screens/doctor_detail_screen.dart
@@ -1,6 +1,6 @@
-import 'dart:typed_data';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:share_plus/share_plus.dart';
